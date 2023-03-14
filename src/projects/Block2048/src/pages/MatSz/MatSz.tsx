1cece3b3af3878b8a9ea7246f392817d49fc4ded
EvGen Gal <genwolk1@gmail.com>
2023-03-14 20:32:00 +0300

    + MatSz2048. Копия с GitHub (без проверки, настр.)

---
+++ src/projects/Block2048/src/pages/MatSz/MatSz.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+
+import { ArrowAccordionFnComp } from "../../../../../Components/ui/accordion/ArrowAccordion.jsx";
+
+import "./MatSz.scss";
+
+// import GithubCorner from 'react-github-corner';
+// import './App.scss';
+
+import { animationDuration, gridGap } from "./config";
+import Header from "./components/Header.tsx";
+import Board from "./components/Board.tsx";
+import Info from "./components/Info.tsx";
+import BoardSizePicker from "./components/BoardSizePicker.tsx";
+
+export const MatSz: React.FC = () => {
+  const [openArrowAccord, setOpenArrowAccord] = useState(false);
+  const handleClickRef = () => {
+    setOpenArrowAccord(!openArrowAccord);
+  };
+
+  return (
+    <div className="MatSz accordion">
+      <div className="MatSz__descript">
+        <h1
+          className={openArrowAccord ? "_active" : ""}
+          onClick={() => {
+            handleClickRef();
+          }}
+        >
+          MatSz
+        </h1>
+        <div className={openArrowAccord ? "openDop" : ""}>
+          <div>
+            Проект на основе{" "}
+            <span>
+              <a
+                href="https://demo.matsz.dev/2048/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                По игре <b>Mat Sz</b> <b>2048 на React, Redux и TS</b>
+              </a>
+            </span>
+          </div>
+          {/* <br />
+          <ol>
+            <li>
+              123
+              <ul>
+                <li>456</li>
+              </ul>
+            </li>
+          </ol> */}
+        </div>
+        <ArrowAccordionFnComp
+          openArrowAccord={openArrowAccord}
+          setOpenArrowAccord={setOpenArrowAccord}
+        />
+      </div>
+      <div
+        className={`MatSz__content--${openArrowAccord ? " openCont" : ""}`}
+        // style={
+        //   {
+        //     '--animation-duration': animationDuration + 'ms',
+        //     '--grid-gap': gridGap,
+        //   } as any
+        // }
+      >
+        <div className="page">
+          <Header />
+          <Board />
+          <BoardSizePicker />
+          <Info />
+        </div>
+      </div>
+    </div>
+  );
+};
+//export {MatSz}
